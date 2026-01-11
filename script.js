6b83d15d921de186308162e642dbc6a0e4f791ad
lawrence <lawrenceknightplus@gmail.com>
2026-01-11 01:48:56 +0000

    build: created plan for code logic

---
+++ script.js
@@ -0,0 +1,24 @@
+// get computers choice
+// get players choice
+
+// set computers && players score in a veriable
+
+
+// play a round
+ // check if player or computer score is === maxscore
+ // if player or computer won return winner msg
+
+ // check who won the round 
+ // if player won return player won and increment player score
+ // else return computer won and increment computers score
+
+
+
+// start game
+  // get computer and players score
+  // play a round
+  // reset game
+
+// reset game
+  // player and computers score
+
