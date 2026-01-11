678c36870cce6e036b902e54f36baf5b49f58473
lawrence <lawrenceknightplus@gmail.com>
2026-01-11 02:27:52 +0000

    update: added play round function

--- script.js
+++ script.js
@@ -25,15 +25,63 @@ let playersScore = 0
 
 
 // play a round
- // check if player or computer score is === maxscore
- // if player or computer won return winner msg
 
- // check who won the round 
- // if player won return player won and increment player score
- // else return computer won and increment computers score
 function playRound(playerChoice, computersChoice){
+  // check if player or computer score is === maxscore
+  let maxScore = 3
+  // if player or computer won return winner msg
+  if(playersScore === maxScore|| computersScore === maxScore){
+    alert(`Winner is ${playersScore === maxScore ? "player" : "computer"}`)
+  }
 
+  // check who won the round 
+  // if player won return player won and increment player score
+  // else return computer won and increment computers score
+
+  if(playerChoice === computersChoice){
+    alert(`draw no one gets a point`)
+  }
+
+  if(playerChoice === "rock" && computersChoice === "paper"){
+    playersScore++
+    alert(`player won this round currentscore : ${playersScore}`)
+  }
+  else if(playerChoice === "paper" && computersChoice === "rock"){
+    computersScore++
+    alert(`player won this round currentscore : ${computersScore}`)
+
+  }
+
+  if(playerChoice === "paper" && computersChoice === "scissors"){
+        playersScore++
+    alert(`player won this round currentscore : ${playersScore}`)
+
+
+  }
+  else if(playerChoice === "scissors" && computersChoice === "paper"){
+        computersScore++
+    alert(`player won this round currentscore : ${computersScore}`)
+
+
+  }
+
+  if(playerChoice === "scissors" && computersChoice === "rock"){
+        playersScore++
+    alert(`player won this round currentscore : ${playersScore}`)
+
+
+  }
+  else if(playerChoice === "rock" && computersChoice === "scissors"){
+        computersScore++
+    alert(`player won this round currentscore : ${computersScore}`)
+
+
+  }
+
+  
 }
+
+// playRound(getPlayersChoice(), getComputersChoice())
 
 
 
